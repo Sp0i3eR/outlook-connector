ff4e56d1b480a9c4c7c0c88e14ce41b5651feef5
Maksim Bokov <maxim.bokov@gmail.com>
2012-01-21 19:53:20 +0400

    Initial

---
+++ src/main/java/ru/hflabs/oss/App.java
@@ -0,0 +1,13 @@
+package ru.hflabs.oss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
