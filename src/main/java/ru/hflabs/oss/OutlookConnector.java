9cd143403207f443ef06730df56f02defaddd0cc
Maksim Bokov <maxim.bokov@gmail.com>
2012-01-25 00:23:49 +0400

    Tap'n'probing my way through Outlook COM model

--- src/main/java/ru/hflabs/oss/OutlookConnector.java
+++ src/main/java/ru/hflabs/oss/OutlookConnector.java
@@ -6,23 +6,39 @@ import org.apache.commons.logging.LogFactory;
 import com.jacob.activeX.ActiveXComponent;
 
 import com.jacob.com.Dispatch;
+import com.jacob.com.SafeArray;
 
 public class OutlookConnector {
 
     private static final Log log =
         LogFactory.getLog(OutlookConnector.class);
+    private static final int olFolderCalendar = 9;
 
     private ActiveXComponent axc;
     private Dispatch outlook;
     private Dispatch ns;
-
+    private Dispatch fldr;
+    private Dispatch appts;
     public OutlookConnector() {
         axc = new ActiveXComponent("Outlook.Application");
         log.info("Outlook ActiveX version: " + axc.getProperty("Version"));
         outlook = axc.getObject();
         log.info("Got Outlook version: " + Dispatch.get(outlook,"Version"));
         ns = axc.getProperty("Session").toDispatch();
-        log.info("Got namespace: " + ns);
+        //ns = Dispatch.call(outlook,"GetNamespace","MAPI").toDispatch();
+        //log.info("Got namespace: " + ns);
+        fldr = Dispatch.call(ns,"GetDefaultFolder",olFolderCalendar).toDispatch();
+        appts =  Dispatch.get(fldr,"Items").toDispatch();
+        log.info("Got item: " + appts.toString());
+        //log.info("Got item: " + Dispatch.get(appt,"Name"));
+
+
+
+    }
+
+    public static void main( String[] args ) {
+            OutlookConnector oc = new OutlookConnector();
+        System.out.println("Done.");
     }
 
 }
